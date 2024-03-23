30dbe7688cbd6135533cd372e4a68f883eb0e307
aagbator <tonjoe99@gmail.com>
2024-03-23 12:28:06 +0100

    feat: Added book card component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
+import { BookCardComponent } from './components/book-card/book-card.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    BookCardComponent
   ],
   imports: [CommonModule, PagesRoutingModule],
 })
