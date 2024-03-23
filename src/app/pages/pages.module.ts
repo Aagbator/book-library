b4d7dc16787d49a157f48278ebcc68e7d509e3ae
aagbator <tonjoe99@gmail.com>
2024-03-23 13:58:17 +0100

    feat: Add page title component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 import { BookCardComponent } from './components/book-card/book-card.component';
+import { PageTitleComponent } from './components/page-title/page-title.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    BookCardComponent
+    BookCardComponent,
+    PageTitleComponent
   ],
   imports: [CommonModule, PagesRoutingModule],
 })
